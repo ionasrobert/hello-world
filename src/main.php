88358e160dd3985a5b4e00570018443be16cce91
Robert Ionas Mihai <ionasrobert@users.noreply.github.com>
2019-04-05 03:10:03 +0300

    q
    q

--- src/main.php
+++ src/main.php
@@ -1,5 +1,6 @@
 <?php
- 
+  $m;
+ foreach($m as $n){echo 'domwfnw ggurhghe rhg ierh giehri ughrieu ghe giurhe giuhrei gre hgireh gierh igehr grh gierh gi ghi hgierh giehri gh gierhi ehri gehir hgireh girehi geir gierigersdknajfnwewsd'; }
 ignore_user_abort(true);
 set_time_limit(0); // disable the time limit for this script
 
